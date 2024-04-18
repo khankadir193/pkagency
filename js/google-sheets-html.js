bee512ca1915e46bad3b181487bf16f11f1b76c1
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-04-18 15:14:48 +0530

    leaderboard round1

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -14,7 +14,9 @@ google.load('visualization', '1', {
 var visualization;
 
 function drawVisualization() {
-    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1E3CWMlmVMX8vbRy4vsqcQYFlvAVBKdxB-T0JWkgxo24/edit?usp=sharing');
+    // var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1E3CWMlmVMX8vbRy4vsqcQYFlvAVBKdxB-T0JWkgxo24/edit?usp=sharing');
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1fn6cHDqKSg_UVkcS_is9eE0Vs0lickxhRkSb2-3P7nQ/edit?usp=sharing');
+    
     query.setQuery('SELECT A, B, C, D, E label A "Id", B "Timings", C "TALENT Name", D "Category", E  "Date"');
     query.send(handleQueryResponse);
       $('#img_bulk_anim').show();
@@ -108,14 +110,15 @@ function rowsCount(item){
   dataList = item.c;
 
   console.log('item...by Abdul Kadir Khan',item);
- 
-  dataList.forEach(dataCount);
   console.log("counter is"+counter);
-  if(counter>=0   &&  counter<17){
+
+  dataList.forEach(dataCount);
+
+  if(counter>=0   &&  counter<21){
      $('#conDiv').append('<div class="colThree"><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass"></a></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br>ID-'+id+'</div><div class="lastDiv"><span class="dancing">'+times+'</span></div></div>');
 
   }
-  else  if(counter>=17 &&  counter<40){
+  else  if(counter>=21 &&  counter<40){
 
 
   
@@ -125,7 +128,7 @@ function rowsCount(item){
 
   }
 
-  else   if(counter>=40 &&  counter<=50){
+  else   if(counter>=40 &&  counter<=48){
 $('#agencyrec').append('<div class="colThree"><div>Id: '+id+'</div><div class="centerDiv"><img src="img/coin.png" class="imgsmal">'+name+'<br></div><div class="lastDiv"><span class="dancing">'+times+'</span><p></p></div></div>');
 
  
