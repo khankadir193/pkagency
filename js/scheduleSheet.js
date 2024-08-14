38e5169226caddecd8b1b1f3d6a6e9fbe40f3ed4
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-14 12:51:20 +0530

    pkagency schedule round2

--- js/scheduleSheet.js
+++ js/scheduleSheet.js
@@ -74,7 +74,7 @@ const getDataFromGoogleSheets = (SHEET_ID,RANGE,SHEET_TITLE,tableNo,round) => {
   getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A150:E152","Dec 23 Agency PK Battle","3","round1");
   getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A154:E156","Dec 23 Agency PK Battle","4","round1");
   // // // round2
-  // getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A118:E120","Dec 23 Agency PK Battle","6","round2");
-  // getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A122:E124","Dec 23 Agency PK Battle","7","round2");
+  getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A159:E161","Dec 23 Agency PK Battle","6","round2");
+  getDataFromGoogleSheets("11ZLpOSrhZISYYzGusflZZA3FcXkznRKfvaO_t9hzKJA","A163:E165","Dec 23 Agency PK Battle","7","round2");
 
 
